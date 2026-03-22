286c7152028443ed8a06668e09a5895a700599fc
Nochang0 <hy040504@kakao.com>
2026-03-22 16:57:03 +0900

    2Grade IoT-Basic-Experiment update (2026-03-22 16:57:03)

--- git.sh
+++ git.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-read -p "커밋 메시지: " msg
+read -p "(IoT기초실험) 커밋 메시지: " msg
 
 # 입력이 비었으면 기본 메시지 사용
 if [ -z "$msg" ]; then
-    msg="2Grade IoT-Basic-Experiment update"
+    msg="2Grade IoT-Basic-Experiment update ($(date '+%Y-%m-%d %H:%M:%S'))"
 fi
 
 
